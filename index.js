0f769145f38c41e90675347d4dca098b12365776
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-04-24 15:00:04 +0300

    make it better

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ app.use(express.urlencoded({ extended: true }));
 // start imports
 const fs = require("fs");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" })
+let dir = "./uploads/"
+const upload = multer({ dest: dir })
 const { readFile } = require("xlsx")
 // end imports
 
 // start code 
-
+app.get("/", (req, res) => res.send("scrapis api"))
 app.post("/api", upload.fields([{ name: "excel", maxCount: 1 }]), async (req, res) => {
     let file = req.files.excel[0];
     if (file.mimetype === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
-        let path = await rename(file);
+        let path = await rename(file); 
         let data = await xlsx(path)
         res.status(200).json({ data });
     }
@@ -24,14 +25,17 @@ app.post("/api", upload.fields([{ name: "excel", maxCount: 1 }]), async (req, re
 async function rename(file) {
     let format = file.originalname.split(".")
     format = format[format.length - 1]
-
     let filename = `${file.path}.${format}`
-    fs.rename(file.path, filename, (err) => { if (err) throw err; });
-    return filename
+    try {
+        await fs.renameSync(file.path, filename)
+        return filename
+    } catch (err) { throw err }
 }
 async function xlsx(path) {
-    let { Sheets } = readFile(path);
-    return Sheets[Object.keys(Sheets)[0]]
+    try {
+        let { Sheets } = readFile(path);
+        return Sheets[Object.keys(Sheets)[0]]
+    } catch (err) { throw err }
 }
 
 // end code
